876ae7c5026ff23d1d7fca59063017635cd53952
JerryJSpringer <JerryJSpringer@gmail.com>
2021-02-05 18:54:13 -0800

    Added comments and removed an unnecessary internal class.

--- Graph/PriorityQueue.cs
+++ Graph/PriorityQueue.cs
@@ -1,24 +1,35 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
 
 namespace DealerOnProblemThree.Graph
 {
+	/// <summary>
+	/// Priority Queue implementation using a min heap. Values can be inserted to
+	/// quickly retrieve the minimum value.
+	/// </summary>
+	/// <typeparam name="T">A type that inherits the IComparable interface.</typeparam>
 	public class PriorityQueue<T> where T :IComparable
 	{
 		private static readonly int INITAL_SIZE = 10;
 
-		private PriorityQueueItem[] _array;
+		private T[] _array;
 		public int Size { get; private set; }
 
+		/// <summary>
+		/// Creates a new priority queue.
+		/// </summary>
 		public PriorityQueue()
 		{
-			_array = new PriorityQueueItem[INITAL_SIZE];
+			_array = new T[INITAL_SIZE];
 			Size = 0;
 		}
 
+		/// <summary>
+		/// Adds an item that is then put into the correct position on the heap.
+		/// </summary>
+		/// <param name="item">The item being added.</param>
 		public void Add (T item)
 		{
-			_array[Size] = new PriorityQueueItem(item);
+			_array[Size] = item;
 
 			// Swaps the new item until it is in the correct position
 			for (int parentIndex = (Size - 1) / 2; parentIndex >= 0; parentIndex = (parentIndex - 1) / 2)
@@ -37,14 +48,19 @@ namespace DealerOnProblemThree.Graph
 				Resize();
 		}
 
+		/// <summary>
+		/// Removes the minimum value from the heap than restructures
+		/// the tree as necessary to maintain minimum value in root.
+		/// </summary>
+		/// <returns>The minimum value in the priority queue.</returns>
 		public T Remove()
 		{
 			if (Size == 0)
 				return default;
 
-			PriorityQueueItem node = _array[0];
+			T node = _array[0];
 			_array[0] = _array[Size - 1];
-			_array[Size - 1] = null;
+			_array[Size - 1] = default;
 			Size--;
 
 			// Used to see if we need to keep swapping
@@ -89,41 +105,29 @@ namespace DealerOnProblemThree.Graph
 				}
 			}
 
-			return node.item;
+			return node;
 		}
 
+		/// <summary>
+		/// Swaps two elements in the array.
+		/// </summary>
+		/// <param name="first">First element being swapped.</param>
+		/// <param name="second">Second element being swapped.</param>
 		private void Swap(int first, int second)
 		{
-			PriorityQueueItem temp = _array[first];
+			T temp = _array[first];
 			_array[first] = _array[second];
 			_array[second] = temp;
 		}
 
+		/// <summary>
+		/// Resizes the array to twice it's current size.
+		/// </summary>
 		private void Resize()
 		{
-			PriorityQueueItem[] newArray = new PriorityQueueItem[Size * 2];
+			T[] newArray = new T[Size * 2];
 			Array.Copy(_array, newArray, Size);
 			_array = newArray;
-		}
-
-		public int CompareTo(object obj)
-		{
-			throw new NotImplementedException();
-		}
-
-		internal class PriorityQueueItem : IComparable<PriorityQueueItem>
-		{
-			internal readonly T item;
-
-			public PriorityQueueItem(T item)
-			{
-				this.item = item;
-			}
-
-			public int CompareTo([AllowNull] PriorityQueueItem other)
-			{
-				return item.CompareTo(other.item);
-			}
 		}
 	}
 }
